921bd70b9cb8455356b03ba74b13b7108a93abbb
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-07-27 10:00:41 +0200

    Update connect2_MCTS.py

--- connect2_MCTS.py
+++ connect2_MCTS.py
@@ -15,15 +15,17 @@ def rand_child(child): # this function creates a random child from the given nod
     state[1] = -state[1]
     return state
 
-def random_simulation(child):
-    # this function randomly simulates the game from the given node to the end, calculates the value of terminal node and
+def random_simulation(child, number_of_simulations):
+    # this function randomly simulates ''n = number_of_simulations'' times the entire game from the given node, calculates the value of terminal node and
     # returns the given node with the updated value and number of visits
     depth = child[0].count(0)
-    childs = [copy.deepcopy(child)]
-    for i in range(depth):
-        childs.append(rand_child(childs[i]))
-    child[2] += value_policy(childs[0])[2]
-    child[3] += 1
+    for j in range(number_of_simulations):
+        childs = [copy.deepcopy(child)]
+        childs[0][2] = 0
+        for i in range(depth):
+            childs.append(rand_child(childs[i]))
+        child[2] += value_policy(childs[0])[2]
+        child[3] += 1
     return child
 
 def value_policy(child): # raises the value of each side: (2 or -2 for 2 in a row, 3 or -3 for 3 in a row and 4 or -4 for 4 in a row)
