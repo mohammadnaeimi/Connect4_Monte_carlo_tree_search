011dc90d56fb6554827611d96d33f7b5e554035d
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-08-08 11:43:22 +0200

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -4,8 +4,8 @@ import math
 
 class Game:
 
-    def __init__(self, initiate): # input value is a list containing >> 0:State - 1:To_play - 2:Number_of_wins - 3:Number_of_visits - 4:UCT_factor
-        self.toplay = initiate[1] # the players who's win is simulated
+    def __init__(self, initiate, to_win): # input value is a list containing >> 0:State - 1:To_play - 2:Number_of_wins - 3:Number_of_visits - 4:UCT_factor
+        self.toplay = to_win # the players who's win is simulated
         self.gamestate = [] # the states of the game which have been visited
         self.number_of_total_simulations = 0
         self.selected = [initiate] # the nodes
@@ -18,24 +18,23 @@ class Game:
         random_ind = random.choice(indice)
         child[0][random_ind] = child[1]
         child[1] = -child[1]
-        #print(child)
         return child
 
-    def random_simulation(self, child, number_of_simulations):
+    def random_simulation(self, number_of_simulations):
         # this function randomly simulates ''n = number_of_simulations times'' the entire game from the given node, calculates the value of terminal node and
         # returns the given node with the updated value and number of visits
-        depth = child[0].count(0)
-        for j in range(number_of_simulations):
-            childs = [copy.deepcopy(child)]
-            #childs[0][2] = 0
-            for i in range(depth):
-                childs.append(self.rand_child(childs[i]))
-            value = self.value_policy(childs[0])[2]
-            if value * self.toplay > 0:
-                #child[2] += value
-                child[2] += 1
-            child[3] += 1
-        return child
+        for child in self.expanded:
+            depth = child[0].count(0)
+            for j in range(number_of_simulations):
+                childs = [copy.deepcopy(child)]
+                for i in range(depth):
+                    childs.append(self.rand_child(childs[i]))
+                value = self.value_policy(childs[0])[2]
+                if value * self.toplay > 0:
+                    child[2] += 1
+                child[3] += 1
+                self.number_of_total_simulations += 1
+        self.number_of_total_simulations += 1
 
     def value_policy(self, child):  # raises the value of each side: (2 or -2 for 2 in a row, 3 or -3 for 3 in a row and 4 or -4 for 4 in a row)
         game_length = len(child[0])
@@ -52,7 +51,7 @@ class Game:
 
 
     def expand(self):  # expands all the possible childs of a given node
-        child = self.selected[-1]
+        child = self.selected[-1] # selects the childs that is going to be expanded
         childs = [copy.deepcopy(child) for i in range(len(child[0]))]
         todelete_indices = []
         for i in range(len(child[0])):
@@ -69,22 +68,22 @@ class Game:
     def backpropogation(self): # updates the selected childs with the simulations
         length = len(self.gamestate)
         for i in range(self.expandindex, length):
-            self.selected[0][2] += self.gamestate[i][2]
-            self.selected[0][3] += self.gamestate[i][3]
+            for j in range(len(self.selected)):
+                self.selected[j][2] += self.gamestate[i][2]
+                self.selected[j][3] += self.gamestate[i][3]
         self.expandindex += self.expandlenght
 
 
-    def selection(self, childs):
+    def selection(self):
         list_value = []
-        for i in childs:
+        for i in self.expanded:
             list_value.append(i[4])
-        result = childs[list_value.index(max(list_value))]
+        result = self.expanded[list_value.index(max(list_value))]
         self.selected.append(result)
-        return result
 
-    def UCB_factor(self, child):
-        child[4] = round((child[2] / child[3]) + math.sqrt(2) * math.sqrt(math.log(self.number_of_total_simulations) / child[3]), 2)
-        return child
+    def UCB_factor(self):
+        for i in self.expanded:
+            i[4] = round((i[2] / i[3]) + math.sqrt(2) * math.sqrt(math.log(self.number_of_total_simulations) / i[3]), 2)
 
     def update(self, state):
         if len(self.gamestate) == 0:
