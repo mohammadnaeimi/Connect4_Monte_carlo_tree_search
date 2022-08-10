06a7ba48529d4f9797416d07a69b0afbdcefcba0
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-08-10 10:49:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from MCTS import Game
 
-def cycle(g):
-    g.expand()
-    g.random_simulation(50)
-    g.UCB_factor()
-    g.selection()
-    g.backpropogation()
-
-def Run_game():
-    g = Game(initial_state, to_win)
-    g.update(initial_state)
-    while g.expandlenght != 1:
-        cycle(g)
-    print(g.selected)
-
-def play(): # runs the game between the user and the computer
+def cycle(child, game):
+    depth = child[0].count(0)
+    for i in range(depth):
+        game.selection(child)
+        game.rollout(5, game.selected[-1])
+        game.backpropogation()
     return
 
+
+def run(): # runs the game between the user and the computer
+    initial_state, to_win = [[0, 0, 0, 0, 0, 0, 0, 0], 1, 0, 0, 0, -1], -1
+    g = Game(initial_state, to_win)
+    while g.check_leaf() is not True:
+        g.expand(g.current_child)
+        cycle(g.current_child, g)
+        g.selection(g.current_child)
+
+    print(g.gamestate)
+    print(g.expanded)
+    print(g.selected)
+    print(g.current_child)
+
+
 if __name__ == '__main__':
-    initial_state, to_win = [[0, 0, 0, 0, 0, 0, 0, 0], 1, 0, 0, 0], 1
-    Run_game()
+    run()
+
