86ea90c12e3ea923b4e690e073871f443cf1f33f
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-08-10 09:47:21 +0200

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -1,17 +1,18 @@
 import random
 import copy
 import math
+from itertools import cycle
 
 class Game:
 
     def __init__(self, initiate, to_win): # input value is a list containing >> 0:State - 1:To_play - 2:Number_of_wins - 3:Number_of_visits - 4:UCT_factor
         self.toplay = to_win # the players who's win is simulated
-        self.gamestate = [] # the states of the game which have been visited
+        self.gamestate = [initiate] # the states of the game which have been visited
         self.number_of_total_simulations = 0
         self.selected = [initiate] # the nodes
-        self.expandindex = 1
-        self.expandlenght = 0
-        self.expanded = 0
+        self.expanded = []
+        self.selection_indice = [0]
+        self.current_child = 0
 
     def rand_child(self, child):  # this function creates a random child from the given node
         indice = [index for (index, item) in enumerate(child[0]) if item == 0]
@@ -19,22 +20,6 @@ class Game:
         child[0][random_ind] = child[1]
         child[1] = -child[1]
         return child
-
-    def random_simulation(self, number_of_simulations):
-        # this function randomly simulates ''n = number_of_simulations times'' the entire game from the given node, calculates the value of terminal node and
-        # returns the given node with the updated value and number of visits
-        for child in self.expanded:
-            depth = child[0].count(0)
-            for j in range(number_of_simulations):
-                childs = [copy.deepcopy(child)]
-                for i in range(depth):
-                    childs.append(self.rand_child(childs[i]))
-                value = self.value_policy(childs[0])[2]
-                if value * self.toplay > 0:
-                    child[2] += 1
-                child[3] += 1
-                self.number_of_total_simulations += 1
-        self.number_of_total_simulations += 1
 
     def value_policy(self, child):  # raises the value of each side: (2 or -2 for 2 in a row, 3 or -3 for 3 in a row and 4 or -4 for 4 in a row)
         game_length = len(child[0])
@@ -49,47 +34,72 @@ class Game:
         child[2] = result
         return child
 
+    def UCB_factor(self, i):
+        if i[3] == 0:
+            i[4] = 100
+        else:
+            i[4] = round((i[2] / i[3]) + math.sqrt(2) * math.sqrt(math.log(self.number_of_total_simulations) / i[3]), 2)
+        return i[4]
 
-    def expand(self):  # expands all the possible childs of a given node
-        child = self.selected[-1] # selects the childs that is going to be expanded
+    def rollout(self, number_of_simulations, child):
+        depth = child[0].count(0)
+        for j in range(number_of_simulations):
+            childs = [copy.deepcopy(child)]
+            for i in range(depth):
+                childs.append(self.rand_child(childs[i]))
+            value = self.value_policy(childs[0])[2]
+            if value * self.toplay > 0:
+                child[2] += 1
+            child[3] += 1
+            self.number_of_total_simulations += 1
+        #self.number_of_total_simulations += 1
+
+    def backpropogation(self): # backpropogates the updated values after rollout (simulation)
+        for i in range(len(self.selection_indice) - 1):
+            self.gamestate[self.selection_indice[i]][2] += self.gamestate[self.selection_indice[i + 1]][2]
+            self.gamestate[self.selection_indice[i]][3] += self.gamestate[self.selection_indice[i + 1]][3]
+        self.selection_indice = [0]
+
+    def expand(self, child):  # expands all the possible childs of a given node
         childs = [copy.deepcopy(child) for i in range(len(child[0]))]
         todelete_indices = []
         for i in range(len(child[0])):
             if childs[i][0][i] == 0:
                 childs[i][0][i] = child[1]
                 childs[i][1] = -child[1]
-                childs[i][2], childs[i][3], childs[i][4] = 0, 0, 0
+                childs[i][2], childs[i][3], childs[i][4], childs[i][-1] = 0, 0, 0, -1
             else:
                 todelete_indices.append(i)
-        self.expanded = [result for i, result in enumerate(childs) if i not in todelete_indices]
+        self.expanded.append([result for i, result in enumerate(childs) if i not in todelete_indices])
         self.update(self.expanded)
-        self.expandlenght = len(self.expanded)
+        child[-1] = self.expanded.index(self.expanded[-1])
 
-    def backpropogation(self): # updates the selected childs with the simulations
-        length = len(self.gamestate)
-        for i in range(self.expandindex, length):
-            for j in range(len(self.selected)):
-                self.selected[j][2] += self.gamestate[i][2]
-                self.selected[j][3] += self.gamestate[i][3]
-        self.expandindex += self.expandlenght
-
-
-    def selection(self):
+    def selection(self, parent): # selects the children of input (parent) with the higher UCB1 factor
         list_value = []
-        for i in self.expanded:
-            list_value.append(i[4])
-        result = self.expanded[list_value.index(max(list_value))]
+        for i in self.expanded[parent[-1]]:
+            list_value.append(self.UCB_factor(i))
+        result = self.expanded[parent[-1]][list_value.index(max(list_value))]
+        for i in self.gamestate:
+            if i[0] == result[0]:
+                self.selection_indice.append(self.gamestate.index(i))
         self.selected.append(result)
+        #self.selected = self.selected[-1:]
+        self.current_child = result
+        return result
 
-    def UCB_factor(self):
-        for i in self.expanded:
-            i[4] = round((i[2] / i[3]) + math.sqrt(2) * math.sqrt(math.log(self.number_of_total_simulations) / i[3]), 2)
+    def check_leaf(self, child): # checks if the child is a terminal node
+        if sum(child) == 0:
+            return True
 
-    def update(self, state):
+    def check_expand(self, input): # checks if the input is a parent or a child: if True the input is a child node (not expanded yet)
+        if input[-1] == True:
+            return True
+
+    def update(self, state): # updates all the visited states of the tree
         if len(self.gamestate) == 0:
             self.gamestate.append(state)
         else:
-            for i in state:
+            for i in state[-1]:
                 self.gamestate.append(i)
 
     def __str__(self, mode):
