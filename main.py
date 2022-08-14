7a5222599a1284bf9a1f9c59e1bf4fa5ebd8ab7d
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-08-14 17:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import model
+from model import model
 from MCTS import Game
 
-    initial_state = [[0, 0, 0, 0, 0, 0, 0, 0], 0, 0, 0, 0, -1]
-    g = Game(initial_state)
-    g.toplay = -1
+initial_state = [[0, 0, 0, 0, 0, 0, 0, 0], 0, 0, 0, 0, -1]
+g = Game(initial_state)
+g.toplay = -1
 
 if __name__ == '__main__':
-    p = model()
+    p = model(initial_state)
+    print(p.board)
